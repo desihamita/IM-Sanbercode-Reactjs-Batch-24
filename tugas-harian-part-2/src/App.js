fbd592574097a91ede05d39d17561c3b1c1e8c88
desihamita <desishmt@gmail.com>
2021-05-18 08:32:56 +0700

    add tugas-11

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React from 'react'
 import IntroReactJS from './Tugas-9/tugas9'
 import TableBuah from './Tugas-10/tugas10'
-import Timer from './Tugas-9/Tugas-11/tugas11'
+import Timer from './Tugas-11/tugas11'
 import './App.css';
 
 function App() {
